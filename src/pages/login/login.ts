a65a458c9e47dacadcd5b03c2981e1353bd2bb45
Abdelazim Ali <blackflower225@gmail.com>
2017-06-09 15:41:13 +0200

    add post to parent add interface for parent

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, MenuController, LoadingController } from 'ionic-angular';
 import { MyChildPage } from '../my-child/my-child';
+import { Storage } from '@ionic/storage';
 
-import { TrackApi } from '../shared/track-api.service';
+import { TrackApi, IParent } from '../shared/track-api.service';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
@@ -18,19 +19,22 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 export class LoginPage {
 
   loginForm: FormGroup;
-  parents: Array<any> = [];
-  selectedParent: any;
+  parents: Array<IParent> = [];
+  selectedParent: IParent;
   msg: string = "";
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private trackApi: TrackApi,
     private loadingCtrl: LoadingController,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private store: Storage,
+    private menuCtrl: MenuController
   ) {
-        this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required,Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')]],
-      password: ['',Validators.required],
+    this.menuCtrl.swipeEnable(false);
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')]],
+      password: ['', Validators.required],
     });
     //this.menuCtrl.enable(false, 'myMenu');
     //Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')
@@ -44,20 +48,22 @@ export class LoginPage {
   GoToMychild() {
     let email = this.loginForm.value.email;
     let pass = this.loginForm.value.password;
-    console.log(email+" - "+pass)
+    console.log(email + " - " + pass)
     let loader = this.loadingCtrl.create({
       content: 'Logging In...',
-      duration:5000
+      duration: 5000,
+      dismissOnPageChange: true
     });
 
     loader.present().then(() => {
       this.trackApi.getParents().subscribe(data => {
-      this.parents = data;
-      this.selectedParent = this.parents.find(p => p.email == email && p.password == pass)
-      if (this.selectedParent != undefined) {
+        this.parents = data;
+        this.selectedParent = this.parents.find(p => p.email == email && p.password == pass)
+        if (this.selectedParent != undefined) {
+          this.store.set('userId', this.selectedParent.id);
           this.navCtrl.setRoot(MyChildPage);
           this.navCtrl.push(MyChildPage, this.selectedParent);
-          loader.dismiss();
+          // loader.dismiss();
         }
         else {
           this.msg = "Wrong Email Or Password";
@@ -68,6 +74,7 @@ export class LoginPage {
 
       })
     })
+
 
 
   }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
-
+import { TrackApi } from '../shared/track-api.service'
 /**
  * Generated class for the SignupPage page.
  *
@@ -13,14 +13,33 @@ import {  NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  parent:any = {
+    "lname": "Zima",
+    "fname": "Zema",
+    "email": "Zima@iti.com",
+    "password": "1544202",
+    "imageUrl": null,
+    "telephone": "01012109629",
+    "address": {
+      "street": "Ay haga",
+      "city": "Ay heta",
+      "country": "Egypt"
+    }
+    
+  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private trackApi:TrackApi) {
     
   }
   
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+    
   }
 
+doWork(){
+  this.trackApi.addParent(this.parent).subscribe(data=>{
+    console.log("data from signUp: "+data)
+  })
+}
 }
