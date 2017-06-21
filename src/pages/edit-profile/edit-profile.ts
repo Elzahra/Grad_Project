cd3c20d91aa2fca3cfc527b6384f9f0f2752c8c8
Elzahra <zahraamer_93@yahoo.com>
2017-06-21 11:35:41 +0200

    remove ima from signup

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -5,8 +5,8 @@ import { Storage } from '@ionic/Storage';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { ParentProfilePage } from '../parent-profile/parent-profile';
 import { LoginPage } from '../login/login';
-//import { Camera, CameraOptions } from '@ionic-native/camera';
-//import firebase from 'firebase';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-edit-profile',
@@ -53,7 +53,7 @@ export class EditProfilePage {
     private loadingCtrl: LoadingController,
     private trackApi: TrackApi,
     private toastCtrl: ToastController,
-    //public cam: Camera,
+    public cam: Camera,
     private formBuilder: FormBuilder) {
     storage.get('parent').then((val) => {
       console.log('parent profileeeeeee ', val);
@@ -61,11 +61,11 @@ export class EditProfilePage {
     });
 
     this.profileForm = this.formBuilder.group({
-      fname: ['', Validators.compose([ Validators.required,Validators.maxLength(15), Validators.pattern('[a-zA-Z ]*')])],
-      lname: ['', Validators.compose([ Validators.required,Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*')])],
-      email: ['', Validators.compose([Validators.required,Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')])],
-      password: ['', Validators.compose([Validators.required,Validators.minLength(4), Validators.maxLength(30)])],
-      telephone: ['', Validators.compose([Validators.required,Validators.pattern('^01([0-9]*)$'), Validators.minLength(11), Validators.maxLength(11)])],
+      fname: ['', Validators.compose([Validators.required, Validators.maxLength(15), Validators.pattern('[a-zA-Z ]*')])],
+      lname: ['', Validators.compose([Validators.required, Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*')])],
+      email: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(30)])],
+      telephone: ['', Validators.compose([Validators.required, Validators.pattern('^01([0-9]*)$'), Validators.minLength(11), Validators.maxLength(11)])],
       street: ['', Validators.compose([Validators.minLength(1), Validators.maxLength(11)])],
       city: ['', Validators.compose([Validators.minLength(1), Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*')])],
       country: ['', Validators.compose([Validators.minLength(1), Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*')])]
@@ -93,103 +93,109 @@ export class EditProfilePage {
     })
   }
   ///////////////////////////
-  // openGallery(): void {
-  //   let cameraOptions = {
-  //     sourceType: this.cam.PictureSourceType.SAVEDPHOTOALBUM,
-  //     destinationType: this.cam.DestinationType.DATA_URL,
-  //     quality: 100,
-  //     targetWidth: 1000,
-  //     targetHeight: 1000,
-  //     encodingType: this.cam.EncodingType.JPEG,
-  //     correctOrientation: true
-  //   }
-  //   this.cam.getPicture(cameraOptions).then(file_uri => {
-  //     this.captureDataUrl = 'data:image/jpeg;base64,' + file_uri;
-  //   }
-  //     , (err) => {
-  //       console.log(err)
-  //     });
-  // }
+  openGallery(): void {
+    let cameraOptions = {
+      sourceType: this.cam.PictureSourceType.SAVEDPHOTOALBUM,
+      destinationType: this.cam.DestinationType.DATA_URL,
+      quality: 100,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: this.cam.EncodingType.JPEG,
+      correctOrientation: true
+    }
+    this.cam.getPicture(cameraOptions).then(file_uri => {
+      this.captureDataUrl = 'data:image/jpeg;base64,' + file_uri;
+    }
+      , (err) => {
+        console.log(err)
+      });
+  }
   ///////////////////////////
-  // openCamera() {
-  //   const cameraOptions: CameraOptions = {
-  //     quality: 100,
-  //     destinationType: this.cam.DestinationType.DATA_URL,
-  //     encodingType: this.cam.EncodingType.JPEG,
-  //     mediaType: this.cam.MediaType.PICTURE,
-  //   };
+  openCamera() {
+    const cameraOptions: CameraOptions = {
+      quality: 100,
+      destinationType: this.cam.DestinationType.DATA_URL,
+      encodingType: this.cam.EncodingType.JPEG,
+      mediaType: this.cam.MediaType.PICTURE,
+    };
 
-  //   this.cam.getPicture(cameraOptions).then((imageData) => {
-  //     // imageData is either a base64 encoded string or a file URI
-  //     // If it's base64:
-  //     this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
-  //   }, (err) => {
-  //     // Handle error
-  //   });
-  // }
+    this.cam.getPicture(cameraOptions).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
   ///////////////////////////
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditProfilePage');
   }
   ////////////////////
   DoneEditProfile() {
-
     console.log("inside DoneEditProfile method");
     let loader = this.loadingCtrl.create({
       content: 'Loading ...',
       duration: 5000,
       dismissOnPageChange: true
     });
-
-          this.parentObj.fname = this.profileForm.value.fname;
-          this.parentObj.lname = this.profileForm.value.lname;
-          this.parentObj.email = this.profileForm.value.email;
-          this.parentObj.password = this.profileForm.value.password;
-          this.parentObj.telephone = this.profileForm.value.telephone;
-          this.parentObj.userRole = Role.Parent;
-          this.parentObj.viewFlag = true;
-         // this.parentObj.imageUrl =snapshot.downloadURL ;
-          this.parentObj.address.city = this.profileForm.value.city;
-          this.parentObj.address.street = this.profileForm.value.street;
-          this.parentObj.address.country = this.profileForm.value.country;
-
-
+if (this.captureDataUrl != "") {
+    let storageRef = firebase.storage().ref();
+    const filename = Math.floor(Date.now() / 1000);
+    const imageRef = storageRef.child(`images/${filename}.jpg`);
+    imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
+      this.parentObj.fname = this.profileForm.value.fname;
+      this.parentObj.lname = this.profileForm.value.lname;
+      this.parentObj.email = this.profileForm.value.email;
+      this.parentObj.password = this.profileForm.value.password;
+      this.parentObj.telephone = this.profileForm.value.telephone;
+      this.parentObj.userRole = Role.Parent;
+      this.parentObj.viewFlag = true;
+      this.parentObj.imageUrl = snapshot.downloadURL;
+      this.parentObj.address.city = this.profileForm.value.city;
+      this.parentObj.address.street = this.profileForm.value.street;
+      this.parentObj.address.country = this.profileForm.value.country;   
+    });
+}
+else{
+    this.parentObj.fname = this.profileForm.value.fname;
+      this.parentObj.lname = this.profileForm.value.lname;
+      this.parentObj.email = this.profileForm.value.email;
+      this.parentObj.password = this.profileForm.value.password;
+      this.parentObj.telephone = this.profileForm.value.telephone;
+      this.parentObj.userRole = Role.Parent;
+      this.parentObj.viewFlag = true;
+      this.parentObj.address.city = this.profileForm.value.city;
+      this.parentObj.address.street = this.profileForm.value.street;
+      this.parentObj.address.country = this.profileForm.value.country;
+}
     loader.present().then(() => {
+{
+      this.trackApi.UpdateParent(this.parentObj).subscribe(data => {
+        if (data) {
+          console.log("inside update parent function", data);
+          this.storage.clear();
+          this.storage.set('parent', data);
+          this.navCtrl.push(ParentProfilePage);
+          loader.dismiss();
 
-     // if (this.captureDataUrl != "") {
-        // let storageRef = firebase.storage().ref();      
-        // const filename = Math.floor(Date.now() / 1000);
-        // const imageRef = storageRef.child(`images/${filename}.jpg`)
-        // imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
-          
+          let toast = this.toastCtrl.create({
+            message: 'Your data is updated successfully',
+            duration: 2500,
+            position: 'middle'
+          });
+          toast.onDidDismiss(() => {
+            console.log('Dismissed toast');
+          });
 
-          this.trackApi.UpdateParent(this.parentObj).subscribe(data => {
-            if (data) {
-              console.log("inside update parent function",data);
-              this.storage.clear();
-              this.storage.set('parent', data);
-              this.navCtrl.push(ParentProfilePage);
-              loader.dismiss();
+          toast.present();
 
-              let toast = this.toastCtrl.create({
-                message: 'Your data is updated successfully',
-                duration: 2500,
-                position: 'middle'
-              });
-              toast.onDidDismiss(() => {
-                console.log('Dismissed toast');
-              });
-
-              toast.present();
-
-            }
-            else {
-              loader.dismiss();
-            }
-          })
-
-       // });
-     // }
-    })
+        }
+        else {
+          loader.dismiss();
+        }
+      })
+      }
+});
   }
 }//class
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { Storage } from '@ionic/Storage';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+//import { Camera, CameraOptions } from '@ionic-native/camera';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { LoginPage } from '../login/login';
 import firebase from 'firebase';
@@ -39,7 +39,7 @@ export class SignupPage {
   //
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public cam: Camera,
+    //public cam: Camera,
     private loadingCtrl: LoadingController,
     private formBuilder: FormBuilder,
 
@@ -59,92 +59,92 @@ export class SignupPage {
 
 
   //
-  openGallery(): void {
-    let cameraOptions = {
-      sourceType: this.cam.PictureSourceType.SAVEDPHOTOALBUM,
-      destinationType: this.cam.DestinationType.DATA_URL,
-      quality: 100,
-      targetWidth: 1000,
-      targetHeight: 1000,
-      encodingType: this.cam.EncodingType.JPEG,
-      correctOrientation: true
-    }
-    this.cam.getPicture(cameraOptions).then(file_uri => {
-      this.captureDataUrl = 'data:image/jpeg;base64,' + file_uri;
-    }
-      , (err) => {
-        console.log(err)
-      });
-  }
+  // openGallery(): void {
+  //   let cameraOptions = {
+  //     sourceType: this.cam.PictureSourceType.SAVEDPHOTOALBUM,
+  //     destinationType: this.cam.DestinationType.DATA_URL,
+  //     quality: 100,
+  //     targetWidth: 1000,
+  //     targetHeight: 1000,
+  //     encodingType: this.cam.EncodingType.JPEG,
+  //     correctOrientation: true
+  //   }
+  //   this.cam.getPicture(cameraOptions).then(file_uri => {
+  //     this.captureDataUrl = 'data:image/jpeg;base64,' + file_uri;
+  //   }
+  //     , (err) => {
+  //       console.log(err)
+  //     });
+  // }
   //
 
-  openCamera() {
-    const cameraOptions: CameraOptions = {
-      quality: 100,
-      destinationType: this.cam.DestinationType.DATA_URL,
-      encodingType: this.cam.EncodingType.JPEG,
-      mediaType: this.cam.MediaType.PICTURE,
-    };
+  // openCamera() {
+  //   const cameraOptions: CameraOptions = {
+  //     quality: 100,
+  //     destinationType: this.cam.DestinationType.DATA_URL,
+  //     encodingType: this.cam.EncodingType.JPEG,
+  //     mediaType: this.cam.MediaType.PICTURE,
+  //   };
 
-    this.cam.getPicture(cameraOptions).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64:
-      this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-    });
-  }
+  //   this.cam.getPicture(cameraOptions).then((imageData) => {
+  //     // imageData is either a base64 encoded string or a file URI
+  //     // If it's base64:
+  //     this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+  //   }, (err) => {
+  //     // Handle error
+  //   });
+  // }
 
   GoToAddChild() {
 
     let loader = this.loadingCtrl.create({
-      content: 'Signing Up...',
-      
+      content: 'Signing Up...',     
     });
     loader.present().then(() => {
-      if (this.captureDataUrl != "") {
-        let storageRef = firebase.storage().ref();
-        // Create a timestamp as filename
-        const filename = Math.floor(Date.now() / 1000);
+      // if (this.captureDataUrl != "") {
+      //   let storageRef = firebase.storage().ref();
+      //   // Create a timestamp as filename
+      //   const filename = Math.floor(Date.now() / 1000);
 
-        // Create a reference to 'images/todays-date.jpg'
-        const imageRef = storageRef.child(`images/${filename}.jpg`);
+      //   // Create a reference to 'images/todays-date.jpg'
+      //   const imageRef = storageRef.child(`images/${filename}.jpg`);
 
-        imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
-          // Do something here when the data is succesfully uploaded!
+      //   imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
+      //     // Do something here when the data is succesfully uploaded!
 
-          this.parentObj.fname = this.SignupForm.value.fname;
-          this.parentObj.lname = this.SignupForm.value.lname;
-          this.parentObj.email = this.SignupForm.value.email;
-          this.parentObj.password = this.SignupForm.value.password;
-          this.parentObj.telephone = this.SignupForm.value.telephone;
-          this.parentObj.address.city = this.SignupForm.value.address;
-          this.parentObj.imageUrl = snapshot.downloadURL;
-          this.trackApi.validateEmail(this.SignupForm.value.email).subscribe(data => {
-          if (data) {
-            console.log("validateEmail>>>>",data)
-            this.isUnique="This Email is Already Registered";
-            this.msg="";
-            loader.dismiss();
-          }
-          else {
-            this.isUnique=""
-            this.trackApi.addParent(this.parentObj).subscribe(data => {
-              if (data) {
-                loader.dismiss();
-                this.navCtrl.push(LoginPage);
-              }
-              else {
-                loader.dismiss();
-                this.msg = 'Somting Went Wrong.. Try Again';
-              }
-            })
-          }
-        })
-        });
+      //     this.parentObj.fname = this.SignupForm.value.fname;
+      //     this.parentObj.lname = this.SignupForm.value.lname;
+      //     this.parentObj.email = this.SignupForm.value.email;
+      //     this.parentObj.password = this.SignupForm.value.password;
+      //     this.parentObj.telephone = this.SignupForm.value.telephone;
+      //     this.parentObj.address.city = this.SignupForm.value.address;
+      //     this.parentObj.imageUrl = snapshot.downloadURL;
+      //     this.trackApi.validateEmail(this.SignupForm.value.email).subscribe(data => {
+      //     if (data) {
+      //       console.log("validateEmail>>>>",data)
+      //       this.isUnique="This Email is Already Registered";
+      //       this.msg="";
+      //       loader.dismiss();
+      //     }
+      //     else {
+      //       this.isUnique=""
+      //       this.trackApi.addParent(this.parentObj).subscribe(data => {
+      //         if (data) {
+      //           loader.dismiss();
+      //           this.navCtrl.push(LoginPage);
+      //         }
+      //         else {
+      //           loader.dismiss();
+      //           this.msg = 'Somting Went Wrong.. Try Again';
+      //         }
+      //       })
+      //     }
+      //   })
+      //   });
 
-      }
-      else {
+      // }
+      // else 
+      //{
         this.parentObj.fname = this.SignupForm.value.fname;
         this.parentObj.lname = this.SignupForm.value.lname;
         this.parentObj.email = this.SignupForm.value.email;
@@ -152,9 +152,7 @@ export class SignupPage {
         this.parentObj.telephone = this.SignupForm.value.telephone;
         this.parentObj.address.city = this.SignupForm.value.address;
         this.parentObj.imageUrl = null;
-        
-           
-            
+          
         this.trackApi.validateEmail(this.SignupForm.value.email).subscribe(data => {
           if (data) {
            
@@ -179,23 +177,9 @@ export class SignupPage {
             loader.dismiss();
           }
         })
-
-
           }
-
-
-
         })
-
-
-
-
-
-
-
-
-
-      }
+     // }
 
     })
   }
